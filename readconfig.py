3be60f19c87d1de0cb3d7084804569a792e38ccb
DESKTOP-36KV42A\guill <guillermoepd@hotmail.com>
2017-09-02 00:36:18 -0300

    get Method: minor syntax fixes

--- readconfig.py
+++ readconfig.py
@@ -177,10 +177,10 @@ class ReadConfig(object):
         if(section in self._sections):
             if(option in self._data[section].keys()):
                 values = self._data[section][option]
-                if(len(values > 1)):
+                if(len(values) > 1):
                     return values
                 else:
-                    return value[0]
+                    return values[0]
         return False
 
     def has_section(self, section):
